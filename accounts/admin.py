d764fa204209d340d23535eb736e615c73b28b54
Raja Muhammad Arbab <rajaarbabthakur1@gmail.com>
2025-08-08 12:15:36 +0500

    Create admin.py

---
+++ accounts/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
+from .models import User
+
+@admin.register(User)
+class UserAdmin(DjangoUserAdmin):
+    list_display = ('id', 'username', 'email', 'role', 'is_staff', 'is_superuser')
+    list_filter = ('role', 'is_staff', 'is_superuser')
+    fieldsets = DjangoUserAdmin.fieldsets + (
+        (None, {'fields': ('role',)}),
+    )
